367682ef3baefc048cf8726d2b58e4848d0aa9df
Moustique <tanguy.leg@gmail.com>
2018-02-28 22:06:50 +0100

    Amélioration CSS PageProfil avec ajout de commentaires

--- js/pageProfil.js
+++ js/pageProfil.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
+
     $(".loader").hide();/*je cache le loader après le chargement de la page*/    
+        // Init Sidenav
+    $('#btnSide').sideNav();
+
+    $('#groupeListe').hide();
+
+    $("#groupeButton").click(function(){
+        var $this = $(this);
+        
+        if($this.data('clicked')) {
+            $this.data('clicked', false);
+            $('#groupeListe').hide();
+        }
+        else {
+            $this.data('clicked', true);
+            $('#groupeListe').show();
+        }
 });
+        
+});
+
