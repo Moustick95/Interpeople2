6fb6d4ac32a7e22d0278bf8cd2901512b56e9e9e
Moustique-Bis <tanguy.leg@gmail.com>
2018-03-02 16:05:56 +0100

    Ajout de modal pour le profil utilisateur

--- js/pageProfil.js
+++ js/pageProfil.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-
-
     $(".loader").hide();/*je cache le loader après le chargement de la page*/    
         // Init Sidenav
     
+        $('.modal').modal();
+
     $('#groupeListe').hide();
 
     $("#groupeButton").click(function(){
@@ -18,5 +18,46 @@ $(document).ready(function(){
             $('#groupeListe').show();
         }
 });
+
+$('#case1').click(function(){
+    $('#boutonDescription').text('Modifier la description');
+    $("#boutonDescription").attr("href", "#modal1");
+    
 });
-$(".button-collapse").sideNav(); 
+
+
+$('#case2').click(function(){
+    $('#boutonDescription').text('Modifier des informations');
+    $("#boutonDescription").attr("href", "#modal2");
+});
+
+
+
+$('#case3').click(function(){
+    $('#boutonDescription').text('Modifier votre compte');
+    $("#boutonDescription").attr("href", "#modal3");
+});
+
+$('#boutonDescription').click(function(){
+
+    if($('#case1').hasClass('active')){
+        $('#modal1').modal('open');
+    }
+
+    if($('#case2').hasClass('active')){
+        $('#modal2').modal('open');
+    }
+
+    if($('#case3').hasClass('active')){
+        $('#modal3').modal('open');
+    }
+
+});
+});
+
+$(".button-collapse").sideNav( ); 
+
+
+
+   
+
