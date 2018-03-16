3cb3c9285d12596a897167f7e87bf47f416dae62
Moustique-Bis <tanguy.leg@gmail.com>
2018-03-16 10:39:56 +0100

    Possibilité d'ajouter des commentaires

--- js/pageMur.js
+++ js/pageMur.js
@@ -1,25 +1,35 @@
 $(document).ready(function () {
 
+    //Materialize pour la boite Option sous Valider
     $('select').material_select();
 
-    
+    $("#nouveauCommentaire").hide();
 
+// Apparition de la boite pour ajouter un commentaire     
+    $('#ajouterCommentaire').click(function () {
+        if ($('#ajouterCommentaire').hasClass('Annuler')) {
+            $("#nouveauCommentaire").hide();
+            $('#ajouterCommentaire').removeClass('Annuler');
+            $('#ajouterCommentaire').html('Ajouter un commentaire');            
+        } else {
+            $('#ajouterCommentaire').html('Annuler');
+            $("#nouveauCommentaire").show();
+            $('#ajouterCommentaire').addClass('Annuler');
+        }
+    });
 
+    //Meme height pour les div dans le nouveau poste
     var result2 = $("#nouveauPoste").height();
     $('#boutonProfil').height(result2);
-
     $(window).resize(function () {
         var result = $("#nouveauPoste").height();
         $('#boutonProfil').height(result);
     });
 
+    //SideNav
     $('#groupeListe').hide();
 
-    (function () {
-        let nCommentaire = $('.fenetreCommentaire').length;
-        $('#nombreCommentaire').html(nCommentaire);
-    })()
-
+    //SideNav
     $("#groupeButton").click(function () {
         var $this = $(this);
 
@@ -33,13 +43,14 @@ $(document).ready(function () {
         }
     });
 
+    //SideNav
     $(".button-collapse").sideNav();
 
+    //Meme hieght du bloc Poste et Commentaire
     $(window).resize(function () {
         var result = $(".cartePoste").height();
         $('.fenetreCommentaire').height(result);
     });
-
     var result = $(".cartePoste").height();
     $('.fenetreCommentaire').height(result);
 
